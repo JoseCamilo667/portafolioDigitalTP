fda369b1cbf61ad3a2ca28a95de0ae45b08e950f
JoseCamilo667 <josecamilomerinomorocho@gmail.com>
2026-07-17 17:59:19 -0500

    Update unidimensional.c

--- unidimensional.c
+++ unidimensional.c
@@ -1,48 +1,22 @@
 #include <stdio.h>
-int main(){
-    int lista [5];
-    lista[0] = 1;
-    lista[1] = 3;
-    lista[2] = 5;
-    lista[3] = 4;
-    lista[4] = 8;
-
-    printf("Posicion 0: %i\n", lista [0]);
-    printf("Posicion 1: %i\n", lista [1]);
-    printf("Posicion 2: %i\n", lista [2]);
-    printf("Posicion 3: %i\n", lista [3]);
-    printf("Posicion 4: %i\n", lista [4]);
-}
-*/
-
-/* otra manera de lita 
-#include <stdio.h>
-int main(){
-    int lista [5] = {1, 3, 5, 4, 8};
-
-    printf("Posicion 0: %i\n", lista [0]);
-    printf("Posicion 1: %i\n", lista [1]);
-    printf("Posicion 2: %i\n", lista [2]);
-    printf("Posicion 3: %i\n", lista [3]);
-    printf("Posicion 4: %i\n", lista [4]);
-}
-
-
-//OTRA FORMA DE ARREGLO UNIDIMENSIONAL ----------------------------------------------
-#include <stdio.h>
-
-
 
 int main(){
-    int lista [5], valor;
-    for (int p = 0; p<5; p++){
-        printf("Ingrese el numero %i: ",p);
-        scanf ("%i",&valor);
-        lista[p]= valor;
+    int notas[5], valor, suma = 0;
+    float promedio;
+
+    for (int p = 0; p < 5; p++){
+        printf("Ingrese la nota %i: ", p);
+        scanf("%i", &valor);
+        notas[p] = valor;
+        suma += valor;
     }
-    
-    for (int i = 0; i<5; i++){
-        printf("El valor de la posicion %i : %i\n", i, lista[i]);
+
+    for (int i = 0; i < 5; i++){
+        printf("La nota en la posicion %i es: %i\n", i, notas[i]);
     }
-    
+
+    promedio = (float)suma / 5;
+    printf("El promedio de las notas es: %.2f\n", promedio);
+
+    return 0;
 }
